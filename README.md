7843667b653f8f32c1b8c0f7303940ff53b6dc80
KhasanBoi <k.kholmatov@student.inha.uz>
2022-04-04 07:51:24 +0500

    'message'

---
+++ README.md
@@ -0,0 +1,27 @@
+# Task manager application
+
+This app allows users to manage the tasks
+
+#### Instructions
+1.Download the source code
+```bash
+git clone [url to the github repo]
+```
+or just download the *.zip file
+
+2.Install deps
+```bash
+npm install
+```
+
+3.Rn the app
+```bash
+node app
+```
+
+#### Deps list
+- express
+- pug
+
+#### Link to GitHub repo
+[url to github repo]
--- app.js
+++ app.js
@@ -1,16 +1,76 @@
-const express = require("express")
+
+// third party libs
+const express = require('express')
 const app = express()
+
+// node libs
+const fs = require('fs')
 const PORT = 8000
 
 app.set('view engine', 'pug')
+// assets
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({extended:false}))
 
 // http://localhost:8000
 app.get('/', (req, res) => {
-    res.render('home')
+    fs.readFile('./data/tasks.json', (err, data)=>{
+        if (err) throw err
+
+        const tasks = JSON.parse(data)
+
+        res.render('home', { tasks: tasks})
+    })
 })
+
+app.post('/add', (req, res) =>{
+    const formData  = req.body
+
+    if (formData.task.trim() == '') {
+        fs.readFile('./data/tasks.json', (err, data)=>{
+            if (err) throw err
+
+            const tasks = JSON.parse(data)
+
+            res.render('home', { error : true, tasks : tasks})
+        })
+    }else{
+        fs.readFile('./data/tasks.json', (err, data) =>{
+            if (err) throw err
+            
+            const tasks = JSON.parse(data)
+
+            const task = {
+                id: id(),
+                task_description: formData.task,
+                done: false 
+            }
+
+            tasks.push(task)
+
+            fs.writeFile('./data/tasks.json', JSON.stringify(tasks), (err)=>{
+                if (err) throw err
+
+                fs.readFile('./data/tasks.json', (err, data)=>{
+                    if (err) throw err
+
+                    const tasks = JSON.parse(data)
+
+                    res.render('home', { success : true, tasks : tasks})
+                })
+            })
+        })
+    }
+})
+
 
 app.listen(PORT, (err) => {
     if (err) throw err
 
     console.log(`This app is running on port ${PORT}`)
 })
+
+
+function id () {
+    return '_' + Math.random().toString(36).substring(2, 9)
+}
